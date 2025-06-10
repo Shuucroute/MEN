fcd3caae649691a9037b641644e918d32e4b1aa9
Shuucroute <ethan.rampnoux@gmail.com>
2025-06-10 14:46:38 +0200

    suite

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import Navbar from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+import Header from './components/Header.vue';
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div class="app-container">
-    <Navbar />
+    <Header />
     <RouterView />
     <Footer />
   </div>
